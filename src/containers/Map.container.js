f0b3904634da0d1ffd0d186c08debfb508984ee8
andrewdmo <andrewdmo@gmail.com>
2019-01-19 12:43:42 -0500

    -ok

--- src/containers/Map.container.js
+++ src/containers/Map.container.js
@@ -34,37 +34,37 @@ export default class MapContainer extends Component {
         // this.interval = setInterval(() => this.tick(), 10000); //in ms
 
 
-        // 1. LIVE location + setState currentCoords:
+        // 1. LIVE location + setState currentCoords: -----------------------------
 
-        // navigator.geolocation.getCurrentPosition((pos) => {
-        //
-        //         this.setState({
-        //             currentCoords: {
-        //                 lat: pos.coords.latitude, lng: pos.coords.longitude,
-        //                 workDate: new Date().toLocaleTimeString()
-        //             },
-        //             updated: true,
-        //             message: 'Live coords are: \n'
-        //         });
-        //
-        //
-        //     }, //end nav.geoLo callback
-        //
-        //     (err) => {
-        //         console.warn(`GeoLocation /\nYour problem Error: \n(${err.code}): ${err.message}`);
-        //         this.setState({message: 'GeoLocation /\nYour problem Error: ' + err.message, updated: false});
-        //
-        //     }, {
-        //         enableHighAccuracy: true,
-        //         timeout:
-        //             10000,
-        //         maximumAge:
-        //             0
-        //     });
+        navigator.geolocation.getCurrentPosition((pos) => {
 
-        // 2. RETRIEVE LATEST Server data:
+                this.setState({
+                    currentCoords: {
+                        lat: pos.coords.latitude, lng: pos.coords.longitude,
+                        workDate: new Date().toLocaleTimeString()
+                    },
+                    updated: true,
+                    message: 'Live coords are: \n'
+                });
 
-        window.fetch('http://localhost:1235/api/coords', {
+
+            }, //end nav.geoLo callback
+
+            (err) => {
+                console.warn(`GeoLocation /\nYour problem Error: \n(${err.code}): ${err.message}`);
+                this.setState({message: 'GeoLocation /\nYour problem Error: ' + err.message, updated: false});
+
+            }, {
+                enableHighAccuracy: true,
+                timeout:
+                    10000,
+                maximumAge:
+                    0
+            });
+
+        // 2. RETRIEVE LATEST Server data: -----------------------------<
+
+        const retrievedCoords = window.fetch('http://localhost:1235/api/coords', {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 // 'Vary': 'Origin', // for specific URLs @ dif IPs
@@ -73,19 +73,60 @@ export default class MapContainer extends Component {
             },
             credentials: "same-origin",
 
-            // mode: "no-cors" //todo CORS back on
+            // mode: "no-cors" //careful with use
         })
             .then(res => res.json())
 
             .then(json => {
                 console.log('previousC.lat fetched from DB:', json.previousCoords.lat);
                 this.setState({
-                    currentCoords: json.previousCoords,
+                    previousCoords: json.previousCoords,
+                    // currentCoords: json.currentsCoords,
                     updated: true,
                     message: 'Retrieved coords are: \n'
                 });
-                console.log('state.previousCoords: ' + this.state.previousCoords.lat);
 
+
+                // 3. POST currentCoords or not: -----------------------------
+
+                if (this.state.currentCoords !== retrievedCoords) {
+                    console.log('this.state.currentCoords.lat: ' + this.state.currentCoords.lat);
+                    console.log('this.state.previousCoords.lat: ' + this.state.previousCoords.lat);
+
+                    console.log('updating DB...');
+
+
+                    // if POST:
+                    window.fetch('http://localhost:1235/api/coords', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            currentCoords: this.state.currentCoords,
+                            // login: 'hubot',
+                        }),
+                        mode: 'no-cors', //TODO: turn off
+                        credentials: "same-origin"
+                    })
+                        .then(res => res.json)
+                        .then(json => {
+                            console.log('DB updated' + json);
+                            this.setState({
+                                updated: true
+                            });
+
+                        })
+
+                        // from POST req
+                        .catch(error => console.log('Coords NOT POSTED to DB: ' + error));
+
+                    // console.log('UPDATE state.coords.lat: ' + this.state.coords.latitude) //leave here
+
+                } // if not
+                else {
+                    console.log('Coords NOT updated as Previous = Current')
+                }
 
             })
 
@@ -93,51 +134,15 @@ export default class MapContainer extends Component {
             .catch(error => console.log('Coords NOT FETCHED from DB: ', error)
             );
 
-// 3. POST currentCoords or not:
 
-        // if (this.state.currentCoords !== this.state.previousCoords) {
-        //     console.log('this.state.currentCoords.lat: ' + this.state.currentCoords.lat);
-        //     console.log('this.state.previousCoords.lat: ' + this.state.previousCoords.lat);
-        //
-        //     console.log('updating DB...');
-        //
-        //
-        //     // if POST:
-        //     window.fetch('http://localhost:1235/api/coords', {
-        //         method: 'POST', headers: {
-        //             'Content-Type': 'application/json'
-        //         },
-        //         body: JSON.stringify({
-        //             currentCoords: this.state.currentCoords,
-        //             // login: 'hubot',
-        //         }),
-        //         mode: 'no-cors', //TODO: turn off
-        //         credentials: "same-origin"
-        //     })
-        //         .then(res => res.json)
-        //         .then(json => {
-        //             console.log('DB updated');
-        //             this.setState({
-        //                 updated: true
-        //             });
-        //
-        //         })
-        //
-        //         // from POST req
-        //         .catch(error => console.log('Coords NOT POSTED to DB: ' + error));
-        //
-        //     // console.log('UPDATE state.coords.lat: ' + this.state.coords.latitude) //leave here
-        //
-        // } // if not
-        // else {
-        //     console.log('Coords NOT updated as Previous = Current')
-        // }
+
 
     } //componentWillMount
 
     componentDidUpdate(prevProps, prevState, snapshot) {
 
-        console.log('mapContainer updated')
+        console.log('MC Updated state.previousCoords.lat: ' + this.state.previousCoords.lat);
+
 
         // navigator.geolocation.getCurrentPosition((pos) => {
         //         console.log('pos ' + pos.coords.latitude + 'prevState: ' + prevState.coords.latitude);
